06b64d7b5219d5d83fa95962da1ca0147dc3768c
Brendan Turner <kisbyturner@gmail.com>
2024-02-03 14:46:45 -0700

    Invalid loging basic features

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import './Login.css';
 
 
 
 const Login: React.FC = () => {
-//function to handle submit
-    const handleLogin = () => {
+    const [userName, setUsername] = useState<string | null> ('');
+    const [password, setPassword] = useState<string | null>('');
+    const [loginFail, setLoginFail] = useState<boolean>(false);
+    const [showPassword, setShowPassword] = useState<boolean>(false);
 
+
+//function to handle submit
+    const handleLogin = (event) => {
+        event.preventDefault()
+        setPassword('')
+        setLoginFail(!loginFail)
         //Cases to cover:
             //Successful Post and returned profile - route to Daily round
             //Successful Post and returned failure - Clear password and indicate incorrect form
@@ -15,8 +23,8 @@ const Login: React.FC = () => {
             return 
         };
   
-    const showPassword = () => {
-        return
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword);
     };
 
     return (
@@ -24,10 +32,17 @@ const Login: React.FC = () => {
             <div className="login-content">
                 <form className="login-form">
                     <label>Username:</label>
-                    <input className="username" type="string"></input>
+                    <input className="username" type="text" value={userName}
+                    onChange={(e) => setUsername(e.target.value)}></input>
                     <label>Password:</label>
-                    <input className="password" type="password" id="passwordInput"></input>
-                    <input type="checkbox" onClick={showPassword}/>Show Password
+                    <input className="password" type={!showPassword ? "password" : "text"} id="passwordInput" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}></input>
+                    <div style={ {"fontSize" : "10px"}}>
+                    <input type="checkbox" onClick={handleTogglePassword}/>Show Password
+                    </div>
+                    <label style={{ "color" : "red", "fontSize" : "10px"}}>
+                    {loginFail ? "Invalid Username or Password" : ''}</label>
                     <button onClick={handleLogin}>Login</button>
                 </form>
             </div>
