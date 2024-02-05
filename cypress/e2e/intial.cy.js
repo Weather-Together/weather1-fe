3380d60c6bd0df99281a48745207f15f3d74b133
butlertree <140225441+butlertree@users.noreply.github.com>
2024-02-05 14:59:15 -0700

    cypress tests for landing page and daily game

--- cypress/e2e/intial.cy.js
+++ cypress/e2e/intial.cy.js
@@ -1,30 +1,54 @@
+//LANDING PAGE
 describe('App Component Tests', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/weather1-fe') // Make sure this is the correct URL where your app is running
   });
 
-  it('passes', () => {
-   
-  });
-
-  it('should get the header', () => {
+  it('should get the basic page display', () => {
+    cy.get('Header').should('contain', 'WeatherTogether');
+    cy.get('Footer').should('contain', 'WeatherTogether');
+    cy.get('nav a').should('contain', 'Login')
+    cy.get('nav a').should('contain', 'New User');
     cy.get('h1').should('contain', 'WeatherTogether');
+    cy.get('.landing-content').should('contain', 'Instructions for the game!!!!!!');
   });
 });
 
 
+
+
+//DAILY GAME GET ON MOUNT
 describe('DailyGame Component', () => {
   beforeEach(() => {
-    // Assuming you have set up your app to run on localhost:3000
-    // Adjust the URL as necessary for your development environment
+    cy.intercept("GET", `https://weather-together-be.onrender.com/api/v0/users/*/rounds/current_daily_round`, {
+      statusCode: 200,
+      fixture: 'dailyGameGet.json'
+    }).as("getDailyGame");
+
+    // After setting up the intercept, visit the page
     cy.visit('http://localhost:3000/weather1-fe/daily-game');
+
+    // Wait for the intercept to ensure it's applied before proceeding
+    cy.wait('@getDailyGame');
   });
 
   it('renders the game interface', () => {
     cy.get('.daily-game').should('exist');
     cy.get('.map-container').should('exist');
     cy.get('.submit-button').should('exist');
+    cy.get('.weather-data-container').should('contain','Today\'s Weather Challenge')
+    cy.get('.weather-data-container').should('contain','Max Temp: 100 °F')
+    cy.get('.weather-data-container').should('contain','Min Temp: 53.4 °F')
+    cy.get('.weather-data-container').should('contain','Avg. Humidity: 88%')
+    cy.get('.weather-data-container').should('contain','Total Precip: 0.24 in')
+
   });
+});
+
+
+
+
+  // Additional tests...
 
   // it('allows a location to be submitted', () => {
   //   // may need to rework
@@ -38,4 +62,3 @@ describe('DailyGame Component', () => {
   //   cy.get('.score-display p').should('contain', 'Your score:1082');
   // });
 
-});
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -13,7 +13,7 @@ import { Routes, Route } from 'react-router-dom';
 
 
 const mockUser = {
-  "id": "403",
+  "id": "404",
   "type": "user",
   "attributes": {
   "email": "user1@gmail.com",
