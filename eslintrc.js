23dd1da1409d1a5e5fee75d01bf256e7ee1b0864
Brendan Turner <kisbyturner@gmail.com>
2024-02-07 23:44:58 -0700

    Lint temp fix

---
+++ eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: ['react-app'], // Use the default ESLint configuration provided by Create React App
+    rules: {
+      // Add or override specific ESLint rules here
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  };
