4a3da9160c95c2864576ff15d9b071b96c3c4c3c
Brendan Turner <kisbyturner@gmail.com>
2024-02-25 17:42:14 -0700

    Local Storage to prevent extra API-calls

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -38,96 +38,102 @@ const Dashboard: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const navigate = useNavigate();
+
   useEffect(() => {
     if(!localStorage.getItem('User')){
       return navigate('../login')
     }
     const storedUser = JSON.parse(localStorage.getItem('User'))
     const fetchRoundData = async () => {
-      try {
-        const response = await fetch(`https://powerful-sierra-25067-22c20bb81d9c.herokuapp.com/api/v0/users/${storedUser.id}/daily_stats`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch daily round data');
+      if(!localStorage.getItem("DailyStats")) {
+        try {
+          const response = await fetch(`https://powerful-sierra-25067-22c20bb81d9c.herokuapp.com/api/v0/users/${storedUser.id}/daily_stats`);
+          if (!response.ok) {
+            throw new Error('Failed to fetch daily round data');
+          }
+          const data = await response.json();
+          
+          const dailyStats = {
+            gameCount: data.daily_stats.daily_game_count, 
+            avgScore: data.daily_stats.average_score_in_daily_games,
+            date: data.daily_stats.date_and_score_of_best_daily_score.date,
+            bestScore: data.daily_stats.date_and_score_of_best_daily_score.score,
+            level1: data.daily_stats.grade_book_daily_round["0.00-500.00"],
+            level2: data.daily_stats.grade_book_daily_round["500.01-1000.00"],
+            level3: data.daily_stats.grade_book_daily_round["1000.01-2000.00"],
+            level4: data.daily_stats.grade_book_daily_round["2000.01-5000.00"],
+            level5: data.daily_stats.grade_book_daily_round["5000.01+"],
+          };
+          setDailyStatsData(dailyStats);
+          localStorage.setItem("DailyStats", JSON.stringify(dailyStats));
+          console.log('Daily stats', localStorage.getItem("DailyStats"))
+          console.log("fetched data", data);
+
+        } catch(error) {
+          setError(error.message);
         }
-        const data = await response.json();
-
-        setDailyStatsData({
-          gameCount: data.daily_stats.daily_game_count, 
-          avgScore: data.daily_stats.average_score_in_daily_games,
-          date: data.daily_stats.date_and_score_of_best_daily_score.date,
-          bestScore: data.daily_stats.date_and_score_of_best_daily_score.score,
-          level1: data.daily_stats.grade_book_daily_round["0.00-500.00"],
-          level2: data.daily_stats.grade_book_daily_round["500.01-1000.00"],
-          level3: data.daily_stats.grade_book_daily_round["1000.01-2000.00"],
-          level4: data.daily_stats.grade_book_daily_round["2000.01-5000.00"],
-          level5: data.daily_stats.grade_book_daily_round["5000.01+"],
-        });
-
-        console.log("fetched data", data);
-
-      } catch(error) {
-        setError(error.message);
       }
+      setDailyStatsData(JSON.parse(localStorage.getItem('DailyStats')));
     };
     const fetchCustomData = async () => {
-      console.log("Fetching custom data...");
-      try {
-        const response = await fetch(`https://powerful-sierra-25067-22c20bb81d9c.herokuapp.com/api/v0/users/${storedUser.id}/games`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch daily round data');
+      if(!localStorage.getItem('CustomGames')) {
+        console.log("Fetching custom data...");
+        try {
+          const response = await fetch(`https://powerful-sierra-25067-22c20bb81d9c.herokuapp.com/api/v0/users/${storedUser.id}/games`);
+          if (!response.ok) {
+            throw new Error('Failed to fetch daily round data');
+          }
+          const responseData = await response.json();
+      
+          // Extract game names from each game object
+          const gameNames = { 
+            names: responseData.data.map((game: any) => game.attributes.game_name)
+          }
+          localStorage.setItem('CustomGames', JSON.stringify(gameNames));
+          console.log("fetched custom data", responseData.data);
+      
+        } catch(error) {
+          setError(error.message);
         }
-        const responseData = await response.json();
-  
-    
-        // Extract game names from each game object
-        const gameNames = responseData.data.map((game: any) => game.attributes.game_name);
-        
-        setCustomGames({
-          names: gameNames // Store game names in an array
-        
-        });
-    
-        console.log("fetched custom data", responseData.data);
-    
-      } catch(error) {
-        setError(error.message);
       }
+      setCustomGames(JSON.parse(localStorage.getItem("CustomGames")));
     };
     
-
-
     const fetchCompetitiveData = async () => {
-      try {
-        const response = await fetch(`https://powerful-sierra-25067-22c20bb81d9c.herokuapp.com/api/v0/users/${storedUser.id}/competitive_stats`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch competitive stats data');
+      if(!localStorage.getItem("CompetitiveData")) {
+        try {
+          const response = await fetch(`https://powerful-sierra-25067-22c20bb81d9c.herokuapp.com/api/v0/users/${storedUser.id}/competitive_stats`);
+          if (!response.ok) {
+            throw new Error('Failed to fetch competitive stats data');
+          }
+          const data = await response.json();
+
+
+          //format the scores in an array
+          const top5scores = data.competitive_stats.top_5_competitive_users
+            .map(user => user.score.toFixed(2))
+            .join(", ");
+
+            //an array of usernames
+          const top5usernames = data.competitive_stats.top_5_competitive_users
+            .map(user => user.username)
+            .join(", ");
+
+          const competitiveData = {
+            top5username: top5usernames,
+            top5score: top5scores,
+            userRank: data.competitive_stats.user_competitive_rank,
+            gameCount: data.competitive_stats.competitive_game_count,
+            avgCompScore: data.competitive_stats.average_score_in_competitive_games.toFixed(2),
+          };
+          localStorage.setItem('CompetitiveData', JSON.stringify(competitiveData))
+          console.log("fetched competitive stats data", data);
+        } catch (error) {
+          setError(error.message)
         }
-        const data = await response.json();
-
-
-        //format the scores in an array
-        const top5scores = data.competitive_stats.top_5_competitive_users
-          .map(user => user.score.toFixed(2))
-          .join(", ");
-
-          //an array of usernames
-        const top5usernames = data.competitive_stats.top_5_competitive_users
-          .map(user => user.username)
-          .join(", ");
-
-        setCompetitiveData({
-          top5username: top5usernames,
-          top5score: top5scores,
-          userRank: data.competitive_stats.user_competitive_rank,
-          gameCount: data.competitive_stats.competitive_game_count,
-          avgCompScore: data.competitive_stats.average_score_in_competitive_games.toFixed(2),
-        });
-
-        console.log("fetched competitive stats data", data);
-        setIsLoading(false)
-      } catch (error) {
-        setError(error.message)
       }
+      setIsLoading(false)
+      setCompetitiveData(JSON.parse(localStorage.getItem("CompetitiveData")));
     };
     fetchRoundData();
     fetchCustomData();
