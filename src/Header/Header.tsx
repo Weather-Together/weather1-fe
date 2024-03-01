3073daab71dce56f54a2e5adc6b18a46425b687e
Sam Tran <samuelttran@gmail.com>
2024-02-29 20:32:46 -0700

    Add logo to header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,6 +3,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ThemeContext } from '../App/App';
 import './Header.css';
+import logo from '../Images/logo_480.png';
 
 const Header: React.FC = () => {
   const { theme, setTheme } = useContext(ThemeContext);
@@ -14,7 +15,10 @@ const Header: React.FC = () => {
   return (
     <header>
       <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-        <h1>WeatherTogether</h1>
+        <div className="grid-title">
+          <div><img src={logo} alt="logo" width="30" height="30" /></div>
+          <div><h1>WeatherTogether</h1></div>
+        </div>
       </Link>
       <nav>
         <Link to="/new-user" className="nav-item">New User</Link>
