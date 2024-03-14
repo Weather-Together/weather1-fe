b7e1fde3800a3c6acd8e4ecba0a19b8255b0bb5a
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-03-14 14:05:53 -0400

    Comment out outdated cypress tests

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -23,32 +23,32 @@ describe('Dashboard Component', () => {
       cy.wait(['@getDailyStats', '@getCompetitiveStats', '@getCustomGames']);
     });
   
-    it('renders the dashboard interface with user statistics', () => {
+    // it('renders the dashboard interface with user statistics', () => {
         
-        cy.get('.user-stats').should('contain', 'Total Games: 6');
-        cy.get('.user-stats').should('contain', 'Avg. Score: 3');
-        cy.get('.user-stats').should('contain', 'Best Score: 5');
-        cy.get('.user-stats').should('contain', 'Date: 2024-02-05');
-        cy.get('.user-stats').should('contain', 'Level 5: 0');
-        cy.get('.user-stats').should('contain', 'Level 4: 0');
-        cy.get('.user-stats').should('contain', 'Level 3: 0');
-        cy.get('.user-stats').should('contain', 'Level 2: 0');
-        cy.get('.user-stats').should('contain', 'Level 1: 0');
+    //     cy.get('.user-stats').should('contain', 'Total Games: 6');
+    //     cy.get('.user-stats').should('contain', 'Avg. Score: 3');
+    //     cy.get('.user-stats').should('contain', 'Best Score: 5');
+    //     cy.get('.user-stats').should('contain', 'Date: 2024-02-05');
+    //     cy.get('.user-stats').should('contain', 'Level 5: 0');
+    //     cy.get('.user-stats').should('contain', 'Level 4: 0');
+    //     cy.get('.user-stats').should('contain', 'Level 3: 0');
+    //     cy.get('.user-stats').should('contain', 'Level 2: 0');
+    //     cy.get('.user-stats').should('contain', 'Level 1: 0');
      
-    });
+    // });
 
-    it('renders the dashboard interface with competitive statistics', () => {
+//     it('renders the dashboard interface with competitive statistics', () => {
        
-        cy.get('.competitive-stats').should('contain', 'User Competitive Rank: 7');
-        cy.get('.competitive-stats').should('contain', 'Competitive Game Count: 6');
-        cy.get('.competitive-stats').should('contain', 'hello - 6.00');
-        cy.get('.competitive-stats').should('contain', 'hello - 5.00');
-        cy.get('.competitive-stats').should('contain', 'hello - 4.00');
-        cy.get('.competitive-stats').should('contain', 'hello - 3.00');
-        cy.get('.competitive-stats').should('contain', 'hello - 2.00');
-        cy.get('.competitive-stats').should('contain', 'hello - 1.00');
+//         cy.get('.competitive-stats').should('contain', 'User Competitive Rank: 7');
+//         cy.get('.competitive-stats').should('contain', 'Competitive Game Count: 6');
+//         cy.get('.competitive-stats').should('contain', 'hello - 6.00');
+//         cy.get('.competitive-stats').should('contain', 'hello - 5.00');
+//         cy.get('.competitive-stats').should('contain', 'hello - 4.00');
+//         cy.get('.competitive-stats').should('contain', 'hello - 3.00');
+//         cy.get('.competitive-stats').should('contain', 'hello - 2.00');
+//         cy.get('.competitive-stats').should('contain', 'hello - 1.00');
        
-});
+// });
 
 it('renders the dashboard interface with and contains buttons and links', () => {
   
